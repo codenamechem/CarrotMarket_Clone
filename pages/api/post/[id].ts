3386a15070463b559f89f8e3503e3956a21ec9be
OSS <seung@OSSui-MacBookPro.local>
2022-04-20 11:52:50 +0900

    ~post detail

---
+++ pages/api/post/[id].ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+
+  const post = await client.post.findUnique({
+    where: {
+      id: +id.toString(), //ts는 query를 string, string[]인식 (catch-all route[...id]) 그래서 string로 변형하고 +붙여 int화
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wondering: true,
+        },
+      },
+    },
+  });
+
+  res.json({ ok: true, post });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -2,13 +2,44 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { Post } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import useMutation from "@libs/client/useMutation";
+import { useEffect } from "react";
+
+interface writeForm {
+  question: string;
+}
+
+interface writeResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<writeForm>();
+  const [post, { loading, data }] = useMutation<writeResponse>("/api/post");
+  const onValid = (data: writeForm) => {
+    if (loading) return;
+    post(data);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router]);
+
   return (
     <Layout canGoBack title="Write Post">
-      <form className="space-y-4 px-4">
-        <TextArea requrid placeholder="Ask a question!" />
-        <Button text="submit" />
+      <form onSubmit={handleSubmit(onValid)} className="space-y-4 px-4">
+        <TextArea
+          register={register("question", { required: true, minLength: 5 })}
+          required
+          placeholder="Ask a question!"
+        />
+        <Button text={loading ? "Loading..." : "submit"} />
       </form>
     </Layout>
   );

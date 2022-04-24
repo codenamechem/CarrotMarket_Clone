2744d28b3ee4d944d5213c41e079412738cf06bf
OSS <seung@Creampapa.local>
2022-04-24 22:30:55 +0900

    message

--- pages/api/streams/[id]/index.ts
+++ pages/api/streams/[id]/index.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
+import messages from "./messages";
 
 async function handler(
   req: NextApiRequest,
@@ -14,6 +15,20 @@ async function handler(
   const stream = await client.stream.findUnique({
     where: {
       id: +id.toString(), //ts는 query를 string, string[]인식 (catch-all route[...id]) 그래서 string로 변형하고 +붙여 int화
+    },
+    include: {
+      message: {
+        select: {
+          id: true,
+          message: true,
+          user: {
+            select: {
+              avatar: true,
+              id: true,
+            },
+          },
+        },
+      },
     },
   });
 
